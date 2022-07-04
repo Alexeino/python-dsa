7a50b6c4c1485647ea90a6acf4d96d934208b47d
Alexeino <ms8750122327@gmail.com>
2022-07-04 11:54:42 +0530

    strings code only

--- List/dic_comprehension.py
+++
@@ -1,2 +0,0 @@
-d1 = {x:f"ID{x}" for x in range(5)}
-print(d1)
--- List/left_rotate.py
+++
@@ -1,8 +0,0 @@
-l = [10,20,30,40]
-
-def left_rotate(l):
-    item = l.pop(0) #O(n)
-    l.append(item) #O(1)
-    return l
-
-print(left_rotate(l))
